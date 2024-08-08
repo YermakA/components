8102c905b7f5306f45f19387ffedaed94268ba2e
YermakA <ermak.080@mail.ru>
2024-08-09 01:37:53 +0800

    feat: add nested

---
+++ nested/app.js
@@ -0,0 +1,20 @@
+Vue.createApp({})
+    .component("plan-picker", {
+        template: "#plan-picker-template",
+        data() {
+            return {
+                plans: ["The Single", "The Curious", "The Addict"],
+            }
+        },
+    })
+    .component("plan", {
+        template: "#plan-template",
+        props: {
+            name: {
+                type: String,
+                required: true,
+            },
+            price: Number,
+        },
+    })
+    .mount("#app")
